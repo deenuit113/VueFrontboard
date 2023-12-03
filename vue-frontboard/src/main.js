9d62d52c24157cbc01d0829f2b51b181cd9e60ed
deenuit113 <leejh5314@gmail.com>
2023-12-03 23:47:33 +0900

    Added Login API

--- vue-frontboard/src/main.js
+++ vue-frontboard/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import './assets/common.css'
 import axios from 'axios'
+import store from './vuex/store'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 //Promise API를 활용하는 HTTP 비동기 통신 라이브러리
 //전역변수로 설정 컴포넌트에서 this.$axios 호출할 수 있다
 app.config.globalProperties.$serverUrl = '//localhost:8081'
-app.use(router).mount('#app')
+app.config.globalProperties.$store = store
+app.use(router).use(store).mount('#app')
