88b3c2ce8795af5505ce7cd2b35281c6fe0e88bb
deenuit113 <leejh5314@gmail.com>
2023-12-04 23:32:58 +0900

    Added Login Token Function

--- vue-frontboard/src/views/common/LoginFn.vue
+++ vue-frontboard/src/views/common/LoginFn.vue
@@ -29,6 +29,7 @@ export default {
       user_pw: ''
     }
   },
+
   methods: {
     ...mapActions(['login']),
     
@@ -45,7 +46,7 @@ export default {
 
       try {
         let loginResult = await this.login({user_id: this.user_id, user_pw: this.user_pw})
-        if(loginResult) alert('로그인 결과: ' + loginResult)
+        if(loginResult) this.goToPages()
       } catch (err) {
         if (err.message.indexOf('Network Error') > -1) {
           alert('서버에 접속할 수 없습니다. 상태를 확인해주세요.')
@@ -54,6 +55,11 @@ export default {
           alert('로그인 정보를 확인할 수 없습니다.')
         }
       }
+    },
+    goToPages() {
+      this.$router.push({
+        name: 'List'
+      })
     }
   },
   computed: {
