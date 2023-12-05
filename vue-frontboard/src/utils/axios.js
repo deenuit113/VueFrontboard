056b5dcef3fcc50fa0416e40c77828a6a7769443
deenuit113 <leejh5314@gmail.com>
2023-12-05 14:41:42 +0900

    Added Loading Bar Function

--- vue-frontboard/src/utils/axios.js
+++ vue-frontboard/src/utils/axios.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import {store} from '@/vuex/store'
 
 axios.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('user_token');
-  config.headers.Authorization = "Bearer " + token;
-  return config;
+    store.commit('LOADING_STATUS', true)
+
+    const token = localStorage.getItem('user_token');
+    config.headers.Authorization = "Bearer " + token;
+    return config;
 });
 
 axios.interceptors.response.use(function (config) {
-  return config
+    store.commit('LOADING_STATUS', false)
+
+    return config
 });
 
 export default axios;
